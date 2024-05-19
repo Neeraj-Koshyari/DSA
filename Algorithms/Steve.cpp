9afce7b39e5bda5809427dea834f04dd8ce79c30
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2024-05-19 11:20:56 +0530

    Create Steve.cpp

---
+++ Algorithms/Steve.cpp
@@ -0,0 +1,16 @@
+//Algorith to find prime number till n
+
+//insted of 2*i we use i*i because till i index they are already marked as non prime;
+
+vector<int> stieve(int n){
+	vector<int> v(n+1,1),prime;
+	v[0] = v[1] = 0;
+
+	for(int i=2;i<=n;i++){
+		if(v[i]){
+			prime.push_back(i);
+			for(int j = i*i; j<=n; j+=i)	v[j] = 0;
+		}
+	}
+	return prime;
+}
