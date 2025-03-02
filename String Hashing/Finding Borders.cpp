fbd558825347e6dce046faeb299295249b88f5ba
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2025-03-02 13:51:59 +0530

    Create Finding Borders.cpp

---
+++ String Hashing/Finding Borders.cpp
@@ -0,0 +1,95 @@
+// Problem Link: https://cses.fi/problemset/task/1732/
+// Solution Link: https://www.youtube.com/watch?v=RoGuguexBUA
+
+
+// This Problem can also be done using z_function
+
+
+// #include<fstream>
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+class Hashing{
+    private:
+    ll invMult(ll x, ll y, ll mod){
+        ll res = 1;
+        while(y){
+            if(y&1) res = (res * x)%mod;
+
+            x = (x*x)%mod;
+            y /= 2;
+        }
+        return res;
+    }
+
+    public:
+    string s;
+    int n;
+    int primes;
+    vector<ll> hashPrimes = {1000000007, 1000000009};
+    const ll base = 31;
+
+    vector<vector<ll>> hashValues, pBase, invPBase;
+    Hashing(string& a){
+        s = a;
+        n = s.length();
+        primes = hashPrimes.size();
+        hashValues.resize(primes, vector<ll>(n));
+        pBase.resize(primes, vector<ll>(n+1));
+        invPBase.resize(primes, vector<ll>(n+1));
+
+        for(int i=0; i<primes; i++){
+            pBase[i][0] = 1;
+            for(int j=1; j<=n; j++){
+                pBase[i][j] = (pBase[i][j-1] * base)%hashPrimes[i];
+            }
+
+            invPBase[i][n] = invMult(pBase[i][n], hashPrimes[i] - 2, hashPrimes[i]);
+            for(int j=n-1; j>=0; j--){
+                invPBase[i][j] = (invPBase[i][j+1] * base)%hashPrimes[i];
+            }
+        }
+
+        for(int i=0; i<primes; i++){
+            for(int j=0; j<n; j++){
+                hashValues[i][j] = ((s[j]-'a' + 1ll) * pBase[i][j]) % hashPrimes[i];
+                if(j>0)
+                    hashValues[i][j] = (hashValues[i][j] + hashValues[i][j-1]) % hashPrimes[i];
+            }
+        }
+    }
+    vector<ll> hashCode(int l, int r){
+        vector<ll> hash(primes);
+        for(int i=0; i<primes; i++){
+            ll num1 = hashValues[i][r];
+            ll num2 = 0;
+
+            if(l > 0)   num2 = hashValues[i][l-1];
+
+            hash[i] = ((num1 - num2 + hashPrimes[i]) % hashPrimes[i]);
+            hash[i] = (hash[i] * invPBase[i][l])%hashPrimes[i];
+        }
+        return hash;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin>>s;
+
+    int n = s.length();
+    Hashing obj(s);
+
+    for(int i=0; i<n-1;i++){
+        if(obj.hashCode(0,i) == obj.hashCode(n-1-i,n-1))    cout<<i+1<<" ";
+    }
+    
+    return 0;
+}
