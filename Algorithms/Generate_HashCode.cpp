133ba7ee59959ceca368eb03694c99c86d3dd424
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2025-03-02 18:19:23 +0530

    Update Generate_HashCode.cpp

--- Algorithms/Generate_HashCode.cpp
+++ Algorithms/Generate_HashCode.cpp
@@ -2,12 +2,13 @@
 
 class Hashing{
     private:
-    ll invMult(ll x, ll y, ll mod){
+    ll modPower(ll x, ll y, ll mod){
         ll res = 1;
-        while(y){
-            if(y&1) res = (res * x)%mod;
 
-            x = (x*x)%mod;
+        while(y){
+            if(y)   res = (res * x) % mod;
+
+            x = (x * x) % mod;
             y /= 2;
         }
         return res;
@@ -15,51 +16,48 @@ class Hashing{
 
     public:
     string s;
-    int n;
-    int primes;
-    vector<ll> hashPrimes = {1000000007, 1000000009};
-    const ll base = 31;
+    ll primes, base = 31, n;
 
-    vector<vector<ll>> hashValues, pBase, invPBase;
-    Hashing(string& a){
+    vector<ll> hashPrimes = {1000000009, 1000000007};
+    vector<vector<ll>> hashValues, baseP, baseInvP;
+
+    Hashing(string &a){
         s = a;
         n = s.length();
         primes = hashPrimes.size();
+
         hashValues.resize(primes, vector<ll>(n));
-        pBase.resize(primes, vector<ll>(n+1));
-        invPBase.resize(primes, vector<ll>(n+1));
+        baseP.resize(primes, vector<ll>(n));
+        baseInvP.resize(primes, vector<ll>(n));
 
         for(int i=0; i<primes; i++){
-            pBase[i][0] = 1;
-            for(int j=1; j<=n; j++){
-                pBase[i][j] = (pBase[i][j-1] * base)%hashPrimes[i];
-            }
+            baseP[i][0] = 1;
+            for(int j=1; j<n; j++)
+                baseP[i][j] = (baseP[i][j-1] * base) % hashPrimes[i];
 
-            invPBase[i][n] = invMult(pBase[i][n], hashPrimes[i] - 2, hashPrimes[i]);
-            for(int j=n-1; j>=0; j--){
-                invPBase[i][j] = (invPBase[i][j+1] * base)%hashPrimes[i];
-            }
+            baseInvP[i][n-1] = modPower(baseP[i][n-1], hashPrimes[i]-2, hashPrimes[i]);
+            for(int j=n-2; j>=0; j--)
+                baseInvP[i][j] = (baseInvP[i][j+1] * base) % hashPrimes[i];
         }
 
         for(int i=0; i<primes; i++){
             for(int j=0; j<n; j++){
-                hashValues[i][j] = ((s[j]-'a' + 1ll) * pBase[i][j]) % hashPrimes[i];
-                if(j>0)
-                    hashValues[i][j] = (hashValues[i][j] + hashValues[i][j-1]) % hashPrimes[i];
+                hashValues[i][j] = ((s[j] - 'a' + 1ll) * baseP[i][j]) % hashPrimes[i];
+
+                if(j>0) hashValues[i][j] = (hashValues[i][j] + hashValues[i][j-1]) % hashPrimes[i];
             }
         }
     }
+
     vector<ll> hashCode(int l, int r){
-        vector<ll> hash(primes);
+        vector<ll> code(primes);
         for(int i=0; i<primes; i++){
-            ll num1 = hashValues[i][r];
-            ll num2 = 0;
+            ll num1 = hashValues[i][r], num2 = 0;
 
             if(l > 0)   num2 = hashValues[i][l-1];
 
-            hash[i] = ((num1 - num2 + hashPrimes[i]) % hashPrimes[i]);
-            hash[i] = (hash[i] * invPBase[i][l])%hashPrimes[i];
+            code[i] = ((num1 - num2 + hashPrimes[i])%hashPrimes[i] * baseInvP[i][l]) % hashPrimes[i];
         }
-        return hash;
+        return code;
     }
 };
