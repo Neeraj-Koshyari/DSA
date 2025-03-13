ad8d52e1a5b4cc5b8fd47362dd824a6409dc31ec
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2025-03-13 20:40:38 +0530

    Update Phone directory.cpp

--- Trie/Phone directory.cpp
+++ Trie/Phone directory.cpp
@@ -1,6 +1,10 @@
 // Problem Link: https://www.geeksforgeeks.org/problems/phone-directory4628/1
 // Solution Link: https://www.youtube.com/watch?v=WafalqRuXJs
 
+
+// ****************    Brute Force    ************** (optimal at line 80 )
+
+// TC O( s.length * n * compare each substring )
 class TrieNode{
 public:
     TrieNode *children[26];
@@ -25,16 +29,6 @@ public:
             node = node->children[idx];
         }
         node->isEnd = true;
-    }
-    
-    bool search(string word) {
-        TrieNode *node = root;
-        for(auto& ch : word){
-            int idx = ch-'a';
-            if(!node->children[idx])    return false;
-            node = node->children[idx];
-        }
-        return node->isEnd;
     }
     
     bool startsWith(string prefix) {
@@ -69,5 +63,76 @@ public:
             for(auto& x: st)    ans[i].push_back(x);
         }
         return ans;
+    }
+};
+
+
+// TC:  O ( ( nLOG(n) * max|contact[i] ) + ( n * max|contact[i] ) + ( |s| * n * max|contact[i] ) ) = |s| * n * max|contact[i]
+
+
+// User function Template for C++
+
+class TrieNode{
+public:
+    TrieNode *children[26];
+    bool isEnd = false;
+    vector<int> ids;
+};
+
+class Trie {
+private:
+    TrieNode *root;
+public:
+    Trie() {       
+        root = new TrieNode();
+    }
+    
+    void insert(string word, int id) {
+        TrieNode *node = root;
+        for(auto& ch : word){
+            int idx = ch-'a';
+            if(!node->children[idx]){
+                node->children[idx] = new TrieNode();
+            }
+            node = node->children[idx];
+            node->ids.push_back(id);
+        }
+        node->isEnd = true;
+    }
+    
+    vector<vector<string>> startsWith(string s, int n, string contact[]) {
+        vector<vector<string>> ans(n);
+        
+        TrieNode *node = root;
+        int i = 0;
+        for( ; i<n; i++){
+            int idx = s[i] - 'a';
+            if(!node->children[idx])    break;
+            node = node->children[idx];
+            for(auto& x : node->ids)   ans[i].push_back(contact[x]);
+        }
+        while(i<n){
+            ans[i].push_back("0");
+            i++;
+        }
+        
+        return ans;
+    }
+};
+
+class Solution{
+public:
+    vector<vector<string>> displayContacts(int n, string contact[], string s)
+    {
+        // code here
+        sort(contact, contact+n);
+        Trie obj;
+        
+        obj.insert(contact[0], 0);
+        for(int i=1;i<n;i++){
+            if(contact[i-1] != contact[i])  obj.insert(contact[i], i);
+        }
+        
+        return obj.startsWith(s, s.length(), contact);
     }
 };
